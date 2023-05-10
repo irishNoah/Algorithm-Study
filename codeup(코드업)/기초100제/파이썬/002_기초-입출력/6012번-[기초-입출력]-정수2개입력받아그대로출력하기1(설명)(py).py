e9f07446de284f9a1979e32d609d8979e1c7f807
Chang-Young Park <parkcy0630@gmail.com>
2023-05-10 11:32:22 +0900

    230510-코드업-기초-입출력

---
+++ codeup(코드업)/기초100제/파이썬/002_기초-입출력/6012번-[기초-입출력]-정수2개입력받아그대로출력하기1(설명)(py).py
@@ -0,0 +1,19 @@
+# 코드업 100제 
+# 6012번-[기초-입출력]-정수2개입력받아그대로출력하기1(설명)(py).py
+
+'''
+줄을 바꿔 정수(integer) 2개를 입력받아 줄을 바꿔 출력해보자.
+
+예시
+a = input() 
+b = input()
+a=int(a)
+b=int(b)
+print(a)
+print(b)
+'''
+
+a = int(input())
+b = int(input())
+print(a)
+print(b)
